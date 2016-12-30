273bc768b66e2f37941af9490da9a03ef9cc91ac
zhangzhoufeng <494074947@qq.com>
2016-12-30 12:22:49 +0800

    [tictactoe] click to add circle

--- [315]tictactoe/game.js
+++ [315]tictactoe/game.js
@@ -1,16 +1,63 @@
-//扩展长按事件
-$.fn.longPress = function(fn){
-	var timeout = undefined;
-	var $this = this;
-	for(var i = 0;i<$this.length;i++){
-		$this[i].addEventListener('touchstart', function(event){
-			timmeout = setTimeout(fn,800); //长按事件超800ms,则执行传入的方法
-		}，false);
+var winKey = [
+	[0,1,2],//横
+	[3,4,5],
+	[6,7,8],
+	[0,3,6],//竖
+	[1,4,7], 
+	[2,5,8], 
+	[0,4,8], //斜
+	[2,4,6]
+];
+var status = [0,0,0,0,0,0,0,0,0]; //都为0
+console.log(status);
 
-		$this[i].addEventListener('touchend', function(event){
-			clearTimeout(timeout); //长按时间少于800ms,不执行传入的方法
-		}，false);
-	}
+
+$(function(){
+	$('.col').on('click', function(event){
+		ripper(event, $(this), 15, 2);
+		//画画
+		draw(0,$(this));
+	});
+});
+
+
+
+//初始化函数
+function init(){
 	
 }
 
+
+//点击画圈或叉
+//flag  0:圈 1：叉
+function draw(flag, ele){
+	// if($(ele).children('.icon').length){
+	// 	return;
+	// }
+	var idx = (/\d/g.exec($(ele).attr('id')))[0]; //获取下标
+	console.log(idx,status,status[idx]);
+	if(status[idx] === 1) {return; }//当前位置上有棋子
+
+	var name = flag? 'cross':'circle';
+	var image = $('<div class="icon icon-' + name +'"></div>');
+	$(ele).append(image);
+	status[idx] = 1; //当前格子有棋子
+}
+
+//点击的水波纹效果
+function ripper(event, ele, radius, time){
+	var ripper = $('<div class="ripper"></div>');
+	var e = event || window.event,
+	    x = e.clientX - ele.offset().left, //相对ele的鼠标的x坐标
+		y = e.clientY - ele.offset().top; //相对ele的鼠标的y坐标
+		ripper.css({
+			'top': y - radius,
+			'left': x - radius
+		});
+	$(ele).append(ripper);
+	setTimeout(function(){
+		ripper.remove();
+	},time *1000);
+
+	
+}
